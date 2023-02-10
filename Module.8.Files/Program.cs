caf3edfecbc29201a969215875e2ac222d937bd2
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-02-10 19:12:52 +0300

    Задание 8.1.4: Добавил класс, который будет предоставлять информацию об установленном в системе диске

--- Module.8.Files/Program.cs
+++ Module.8.Files/Program.cs
@@ -2,11 +2,25 @@
 
 namespace Module._8.Files
 {
+    class Disk
+    {
+        public string Name { get; set; }
+        public long Volume { get; set; }
+        public long FreeSpace { get; set; }
+
+        public Disk(string Name, long Volume, long FreeSpace)
+        {
+            this.Name = Name;
+            this.Volume = Volume;
+            this.FreeSpace = FreeSpace;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Disk disk = new Disk("C", 1024, 512);
         }
     }
 }
