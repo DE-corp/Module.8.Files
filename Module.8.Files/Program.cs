61c91b9e7515778da479570bac479178bee608ae
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-02-10 19:02:29 +0300

    Добавьте файлы проекта.

---
+++ Module.8.Files/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module._8.Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
