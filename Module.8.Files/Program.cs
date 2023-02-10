5df12044136a8cb911185dbd8e29b9fc441338c3
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-02-10 19:30:58 +0300

    Задание 8.1.5: Написал метод, который создает новые папки на диске

--- Module.8.Files/Program.cs
+++ Module.8.Files/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Module._8.Files
 {
@@ -8,19 +9,41 @@ namespace Module._8.Files
         public long Volume { get; set; }
         public long FreeSpace { get; set; }
 
+        public Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();
+
         public Disk(string Name, long Volume, long FreeSpace)
         {
             this.Name = Name;
             this.Volume = Volume;
             this.FreeSpace = FreeSpace;
         }
+
+        public void CreateFolder(string FolderName)
+        {
+            Folders.Add(FolderName, new Folder());
+        }
+
+    }
+
+    public class Folder
+    {
+        public List<string> Files { get; set; } = new List<string>();
+        public string Name { get; set; }
+
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Disk disk = new Disk("C", 1024, 512);
+            var disk = new Disk("C", 1024, 512);
+            disk.CreateFolder("Новая папка");
+            disk.CreateFolder("Новая папка1");
+            disk.CreateFolder("Новая папка2");
+            disk.CreateFolder("Новая папка3");
+
+            Console.WriteLine(disk.Folders.Count);
+
         }
     }
 }
