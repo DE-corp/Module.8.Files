5b0c8c7ae4a431b3c75c0661ae4f58c176510674
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-02-13 21:05:44 +0300

    Задание 8.4.3: Сериализация обьекта

--- Module.8.Files/Program.cs
+++ Module.8.Files/Program.cs
@@ -1,33 +1,46 @@
 ﻿using System.IO;
 using System;
-
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Module._8.Files
 {
+    [Serializable]
+    class Contact
+    {
+        public string Name { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public Contact(string name, long phoneNumber, string email)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+    }
+
     class Program
     {
-        static readonly string FilePath = "BinaryFile.bin";
-
-        static void BinaryRead()
-        {
-            if (File.Exists(FilePath))
-            {
-                using BinaryReader br = new(new FileStream(FilePath, FileMode.Open));
-                Console.WriteLine(br.ReadString());
-            }
-        }
-
-        static void BinaryWrite()
-        {
-            using BinaryWriter br = new(new FileStream(FilePath, FileMode.Open));
-            br.Write($"Файл изменен {DateTime.Now} на компьютере c ОС {Environment.OSVersion}");
-        }
 
         static void Main(string[] args)
         {
-            BinaryWrite();
-            BinaryRead();
+            var Contact = new Contact("Дмитрий", 79992338869, "crazygoup777@gmail.com");
+
+            var Formatter = new BinaryFormatter();
+
+            using (var fs = new FileStream("Contacts.dat", FileMode.OpenOrCreate))
+            {
+                Formatter.Serialize(fs, Contact);
+                Console.WriteLine("Объект сериализован");
+            }
+
+            using (var fs = new FileStream("Contacts.dat", FileMode.OpenOrCreate))
+            {
+                var newContact = (Contact)Formatter.Deserialize(fs);
+                Console.WriteLine("Объект десериализован");
+                Console.WriteLine($"Имя: {newContact.Name} --- Телефон: {newContact.PhoneNumber}");
+            }
         }
-    }
+    }  
 }
 
