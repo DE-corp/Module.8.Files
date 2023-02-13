c28a220f5b2f331748f030fbab1974f1ab45f499
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-02-13 20:32:57 +0300

    Задание 8.4.2: Запись в бинарный файл

--- Module.8.Files/Program.cs
+++ Module.8.Files/Program.cs
@@ -6,17 +6,27 @@ namespace Module._8.Files
 {
     class Program
     {
+        static readonly string FilePath = "BinaryFile.bin";
+
+        static void BinaryRead()
+        {
+            if (File.Exists(FilePath))
+            {
+                using BinaryReader br = new(new FileStream(FilePath, FileMode.Open));
+                Console.WriteLine(br.ReadString());
+            }
+        }
+
+        static void BinaryWrite()
+        {
+            using BinaryWriter br = new(new FileStream(FilePath, FileMode.Open));
+            br.Write($"Файл изменен {DateTime.Now} на компьютере c ОС {Environment.OSVersion}");
+        }
 
         static void Main(string[] args)
         {
-            string FilePath = "BinaryFile.bin";
-            if (File.Exists(FilePath))
-            {
-                using (BinaryReader br = new BinaryReader(new FileStream(FilePath, FileMode.Open)))
-                {
-                    Console.WriteLine(br.ReadString());
-                }
-            }
+            BinaryWrite();
+            BinaryRead();
         }
     }
 }
